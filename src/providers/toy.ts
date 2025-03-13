14dda074a19f95bcb97e3eda15b4e6295fd1a251
brownrw8 <rwbrown@roadrunner.com>
2025-03-13 13:14:24 -1000

    add proxy

--- src/providers/toy.ts
+++ src/providers/toy.ts
@@ -1,13 +1,32 @@
 import { LanguageProvider } from "../interfaces/language";
 import { translate } from '@vitalets/google-translate-api';
+import { HttpProxyAgent } from 'http-proxy-agent';
 
 // This is not a good practice to use Google Translate API for free.
 // This is just for demonstration purposes.
 // You should use the official Google Translate API for production.
 export class ToyProvider implements LanguageProvider {
+  private proxyAgent: HttpProxyAgent<string>|undefined;
+
+  constructor(proxy?: string) {
+    if(proxy) {
+      this.proxyAgent = new HttpProxyAgent(proxy);
+    }
+  }
 
   async translateText(text: string, from: string, to: string): Promise<string> {
-    const translation = await translate(text, { to });
+    let translation = { text: 'NA' };
+    try {
+      translation = await translate(text, { to });
+    } catch (e) {
+      if ((e as any).name === 'TooManyRequestsError') {
+        if(this.proxyAgent){
+          translation = await translate(text, { to, fetchOptions: {agent: this.proxyAgent} });
+        } else {
+          throw new Error('Too many requests. Please try again later.');
+        }
+      }
+    }
     return translation.text;
   }
 }
